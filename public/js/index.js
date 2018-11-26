0f7f7b8ed2b208232202f9372ae7867de2d6544c
user1 <user1@MacBook-Air-Admin.local>
2018-11-26 02:15:55 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-//sends data from the client to the server 
-    socket.emit('createMessage', {
-        to: 'jen',
-        text: 'Hey, this is bla bla bla.'
-    });
 });
 
 socket.on('disconnect', function () {
