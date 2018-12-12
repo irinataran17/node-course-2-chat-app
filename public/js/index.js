7ea191ea1ddee8b19e975b02b9e42d63ede01415
user1 <user1@MacBook-Air-Admin.local>
2018-12-12 02:11:05 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,19 +26,21 @@ $(document).ready(function() {
         li.text(`${message.from}: `);
         a.attr('href', message.url);
         li.append(a);
-        
+
         $('#messages').append(li);
     });
 
 
     $('#message-form').on('submit', function(e){
         e.preventDefault();
+
+        var messageTextbox = $('[name=message]');
     
         socket.emit('createMessage', {
             from: 'User',
-            text: $('[name=message]').val()
+            text: messageTextbox.val()
         }, function () {
-    
+            messageTextbox.val('');
         });
     });
 
@@ -49,12 +51,16 @@ $(document).ready(function() {
             return alert('Geolocation not supported by your browser');
         }
 
+        locationButton.attr('disabled', 'disabled').text('Sending location...');
+
         navigator.geolocation.getCurrentPosition(function (position) {
+            locationButton.removeAttr('disabled').text('Send location');
             socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
         }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
             alert('Unable to fetch location');
         });
     });
